c5eb8e4797b53aa6db992b80ef3bd20e557d390d
Ade Kurniawan <145414449+adkurnwn@users.noreply.github.com>
2026-06-01 12:54:34 +0700

    feat: add cv template

--- app/usecase/system_setting/init.go
+++ app/usecase/system_setting/init.go
@@ -2,6 +2,7 @@ package system_setting
 
 import (
 	"context"
+	"mime/multipart"
 	"time"
 
 	"github.com/adkurnwn/gigsourcehub-general-api/domain"
@@ -12,20 +13,24 @@ import (
 type Usecase interface {
 	Fetch(ctx context.Context) (*gorm_model.SystemSetting, error)
 	Update(ctx context.Context, req request_model.UpdateSystemSettingRequest) error
+	UploadCVTemplate(ctx context.Context, fileHeader *multipart.FileHeader) (string, error)
 }
 
 type appUsecase struct {
 	repo           domain.GormRepo
+	storageRepo    domain.StorageRepo
 	contextTimeout time.Duration
 }
 
 type RepoInjection struct {
-	GormDbRepo domain.GormRepo
+	GormDbRepo  domain.GormRepo
+	StorageRepo domain.StorageRepo
 }
 
 func NewAppUsecase(repo RepoInjection, timeout time.Duration) Usecase {
 	return &appUsecase{
 		repo:           repo.GormDbRepo,
+		storageRepo:    repo.StorageRepo,
 		contextTimeout: timeout,
 	}
 }
